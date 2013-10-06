86973f3d69afe4fe9c64ac601bae5af96f7d1ed6
martinrusev <martin@amon.cx>
2013-10-06 21:06:27 +0300

    Apache plugin

---
+++ amonagent/plugins/apache.py
@@ -0,0 +1,12 @@
+http://www.apache.org/server-status?auto
+
+
+class AmonApachePlugin(object):
+
+
+	def __init__(self):
+		pass
+
+
+	def build_report(self):
+		pass
--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ from setuptools import setup, find_packages
 from amonagent import __version__
  
 setup(
-    name='amonagent',
-    version=__version__,
-    description="Linux system and process information collector",
-    author='Martin Rusev',
-    author_email='martin@amon.cx',
-    zip_safe=False,
+	name='amonagent',
+	version=__version__,
+	description="Linux system and process information collector",
+	author='Martin Rusev',
+	author_email='martin@amon.cx',
+	zip_safe=False,
 	packages=find_packages(),
-    install_requires=['requests==2.0.0'],
+	 data_files=[
+		('/etc', ['amon-agent.conf']),
+		('/etc/init.d', ['amon-agent'])]
+	 )
+	install_requires=['requests==2.0.0'],
 ) 
