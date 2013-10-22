83773bddd0454668be38a02491221fede1770eae
Martin Rusev <martinrusev@zoho.com>
2013-10-22 17:20:36 +0300

    Nginx plugin syntax improvements

--- amonagent/plugins/nginx.py
+++ amonagent/plugins/nginx.py
@@ -18,7 +18,29 @@ class AmonNginxPlugin(object):
 		
 
 	def build_report(self):
-		report = {}
+		# Report format 
+		# {
+		# "plugin": "nginx",
+		# "graphs": [
+		# 	{
+		# 		"name": "Connections",
+		# 		"data": {
+		# 			"connections": 1, 
+		# 			"reading": 0,
+		# 			"writing": 0,
+		# 			"waiting": 0
+		# 	}},
+		# 	{	
+		# 		"name": "Requests per second",
+		# 		"data":  {
+		# 			"requests_per_second": 2
+		# 	}}
+		# ]
+		# }
+		report = {"plugin": "nginx", "graphs": []}
+		temp_report = {} # Used for internal storage
+		graphs = ['Requests per second', 'Connections']
+
 		result = requests.get(self.url)
 		whitelist = ['accepts','handled','requests']
 
@@ -28,26 +50,41 @@ class AmonNginxPlugin(object):
 				stats_line = re.match('\s*(\d+)\s+(\d+)\s+(\d+)\s*$', line)
 				if stats_line:
 					for i, key in enumerate(whitelist): # Enumerate start is supported only in Python 2.6+
-						report[key] = int(stats_line.group(i+1))
+						temp_report[key] = int(stats_line.group(i+1))
 						
 				else:
 					for (key,val) in re.findall('(\w+):\s*(\d+)', line):
 						filtered_key = slugify(key)
-						report[filtered_key] = int(val)
+						temp_report[filtered_key] = int(val)
 		
 		# Calculate requests per second
-		if len(report) > 0:
-			total_requests = report.get('requests', 0)
-			handled = report.get('handled', 0)
+		if len(temp_report) > 0:
+			total_requests = temp_report.get('requests', 0)
+			handled = temp_report.get('handled', 0)
 			
 			if total_requests > 0 and handled > 0:
 				requests_per_second = total_requests/handled
 				if requests_per_second > 0:
 					for key in whitelist:
-						del report[key]
+						del temp_report[key]
 					
-					report['requests_per_second'] = requests_per_second
-					
+					temp_report['requests_per_second'] = requests_per_second
+			
+		report['graphs'].append({"name": "Connections", 
+			"data": {	
+				"connections": temp_report['connections'],
+				"reading": temp_report['reading'],
+				"writing": temp_report['writing'],
+				"waiting": temp_report['waiting']
+			}
+		})
+
+		report['graphs'].append({"name": "Requests per second",
+			"data": {
+				"requests_per_second": temp_report['requests_per_second']
+			}
+		})	
+	
 		return report
 
 
